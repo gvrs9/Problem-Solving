730e17d0aefe3ee7c360c7e564b604e47d38f9fe
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-07 02:20:43 +0530

    Time: 7 ms (85.32%), Space: 13.5 MB (85.42%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -24,4 +24,13 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         return {binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
+    /* // or
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto it1 = std::lower_bound(nums.begin(), nums.end(), target);
+        auto it2 = std::lower_bound(nums.begin(), nums.end(), target + 1);
+        if (it1 != nums.end() && *it1 == target) 
+            return {it1 - nums.begin(), it2 - nums.begin() - 1};
+        return {-1, -1};
+    }
+    */
 };
