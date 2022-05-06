7a6a9ab058d96d0e4d4bcd228dc7660dfb51cca3
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-07 02:18:23 +0530

    Time: 7 ms (85.32%), Space: 13.7 MB (65.64%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,27 @@
 class Solution {
 private:
-    int findFirst(vector<int>& nums, int target) {
+    int binarySearch(vector<int>& nums, int target, bool flag) {
         int lo = 0, hi = nums.size() - 1;
         int res = -1;
         while(lo <= hi) {
             int mid = lo + (hi - lo) / 2;
             if(nums[mid] < target) {
                 lo = mid + 1;
-            }else if(nums[mid] > target) {
+            }else if(nums[mid] > target){
                 hi = mid - 1;
             }else {
                 res = mid;
-                hi = mid - 1;
-            }
-        }
-        return res;
-    }
-    int findLast(vector<int>& nums, int target) {
-        int lo = 0, hi = nums.size() - 1;
-        int res = -1;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(nums[mid] < target) {
-                lo = mid + 1;
-            }else if(nums[mid] > target) {
-                hi = mid - 1;
-            }else {
-                res = mid;
-                lo = mid + 1;
+                if(flag) {
+                    hi = mid - 1;
+                }else {
+                    lo = mid + 1;
+                }
             }
         }
         return res;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {findFirst(nums, target), findLast(nums, target)};
+        return {binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
 };
