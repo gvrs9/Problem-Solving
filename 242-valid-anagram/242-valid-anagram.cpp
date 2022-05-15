1fd590194c6d8f9f213b4cb2543e6270c86d4aa9
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-16 02:09:19 +0530

    Time: 8 ms (79.35%), Space: 7.3 MB (47.00%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,7 +4,11 @@ public:
         if(s.size() != t.size()) return false;
         int count[26] = {0};
         for(int i = 0; i < (int)s.size(); i++) count[s[i] - 'a']++, count[t[i] - 'a']--;
-        for(auto& x : t) if(count[x - 'a']) return false;
+        for(auto& x : count) cout << x << " ";
+        cout << "\n";
+        // for(auto& x : t) if(count[x - 'a']) return false;
+        // if(-10000000000) cout << "hello" << "\n"; this is to show that only 0 and false will not execute and rest all will execute in c++!!
+        for(auto& x: count) if(x) return false;
         return true;
     }
 };
