8dc7b86173aef957640531364fb3657c6301e077
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-16 01:58:30 +0530

    Time: 8 ms (79.35%), Space: 7.3 MB (78.71%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        char count[26] = {0};
+        int count[26] = {0};
         for(int i = 0; i < (int)s.size(); i++) count[s[i] - 'a']++, count[t[i] - 'a']--;
         for(auto& x : t) if(count[x - 'a']) return false;
         return true;
