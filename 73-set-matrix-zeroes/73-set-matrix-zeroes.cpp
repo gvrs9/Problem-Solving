fc1c122cc04176648010c00068cb0d541903bb4b
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-17 02:29:50 +0530

    Time: 9 ms (95.76%), Space: 13.3 MB (19.23%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,19 +1,38 @@
 class Solution {
-private:
-    void fillZeroes(vector<vector<int> >& v, int i, int j, int m, int n) {
-        for(int k = 0; k < m; k++) v[k][j] = 0;
-        for(int k = 0; k < n; k++) v[i][k] = 0;
-    }
 public:
     void setZeroes(vector<vector<int> >& v) {
-        vector<pair<int, int> > vp;
-        for(int i = 0; i < (int)v.size(); i++)
-            for(int j = 0; j < (int)v[0].size(); j++)
-                if(!v[i][j])
-                    vp.push_back({i, j});
-        for(int i = 0; i < (int)vp.size(); i++) {
-            int ind1 = vp[i].first, ind2 = vp[i].second;
-            fillZeroes(v, ind1, ind2, v.size(), v[0].size());
+        int m = v.size(), n = v[0].size();
+        // bool rows[m] = {false}, cols[n] = {false};
+        
+        vector<bool> rows(m, false), cols(n, false);
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(v[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        /*
+        for(bool x : cols) {
+            if(x)
+                cout << 1 << " ";
+            else
+                cout << 0 << " ";
+        }
+        cout << "\n";
+        for(bool x : rows) {
+            if(x)
+                cout << 1 << " ";
+            else
+                cout << 0 << " ";
+        }
+        */
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(rows[i] || cols[j]) v[i][j] = 0;
+            }
         }
     }
 };
