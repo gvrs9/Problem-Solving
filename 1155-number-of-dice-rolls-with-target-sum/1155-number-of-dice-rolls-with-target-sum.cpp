7bbba498971ad393194629ce8c21f5b88aacdfb3
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-20 02:51:38 +0530

    Time: 12 ms (94.18%), Space: 6 MB (86.35%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,8 +1,9 @@
 #define MOD 1000000007
 
 class Solution {
+    int dp[31][1001];
 private:
-    int getRes(int d, int f, int t, vector<vector<int> >& dp) {
+    int getRes(int d, int f, int t) {
         if(d == 0 || t == 0) return d == t;
         if(d <= 0 || t <= 0) return 0;
         if(dp[d][t] != -1) return dp[d][t];
@@ -10,13 +11,13 @@ private:
         int res = 0;
 
         for(int i = 1; i <= f; i++)
-            res = ((res % MOD) + (getRes(d - 1, f, t - i, dp) % MOD)) % MOD;
+            res = ((res % MOD) + (getRes(d - 1, f, t - i) % MOD)) % MOD;
         
         return dp[d][t] = res;
     }
 public:
     int numRollsToTarget(int d, int f, int t) {
-        vector<vector<int> > dp(31, vector<int>(1001, -1));
-        return getRes(d, f, t, dp);
+        memset(dp, -1, sizeof(dp));
+        return getRes(d, f, t);
     }
 };
