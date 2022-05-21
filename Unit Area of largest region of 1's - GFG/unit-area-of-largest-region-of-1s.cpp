f792d60b2d52907f2b5be301895c61e97c43e345
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-22 04:46:34 +0530

    Added solution - LeetHub

--- Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
+++ Unit Area of largest region of 1's - GFG/unit-area-of-largest-region-of-1s.cpp
@@ -5,16 +5,15 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
     private:
-        bool isSafe(vector<vector<int> >& grid, vector<vector<bool> >& visited, int row, int col, int m, int n) {
-            return (row >= 0) && (row < m) && (col >= 0) && (col < n) && (grid[row][col] && !visited[row][col]);
+        bool isSafe(vector<vector<int> >& grid,int row, int col, int m, int n) {
+            return (row >= 0) && (row < m) && (col >= 0) && (col < n);
         }
-        void dfs(vector<vector<int> >& grid, vector<vector<bool> >& visited, int i, int j, int& indiCnt, int m, int n) {
-            visited[i][j] = true;
-            
+        void dfs(vector<vector<int> >& grid,int i, int j, int& indiCnt, int m, int n) {
             for(int k = 0; k < 8; k++) {
-                if(isSafe(grid, visited, i + vp[k].first, j + vp[k].second, m, n)) {
+                if(isSafe(grid, i + vp[k].first, j + vp[k].second, m, n) && grid[i + vp[k].first][j + vp[k].second]) {
+                    grid[i + vp[k].first][j + vp[k].second] = 0;
                     indiCnt++;
-                    dfs(grid, visited, i + vp[k].first, j + vp[k].second, indiCnt, m, n);
+                    dfs(grid, i + vp[k].first, j + vp[k].second, indiCnt, m, n);
                 }
             }
         }
@@ -22,15 +21,14 @@ class Solution {
     vector<pair<int, int> > vp = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     int findMaxArea(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<bool> > visited(m, vector<bool>(n, false));
         int maxCnt = INT_MIN;
-        // vp = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] && !visited[i][j]) {
+                if(grid[i][j]) {
+                    grid[i][j] = 0;
                     int indiCnt = 1;
-                    dfs(grid, visited, i, j, indiCnt, m, n);
+                    dfs(grid, i, j, indiCnt, m, n);
                     maxCnt = max(maxCnt, indiCnt);
                 }
             }
