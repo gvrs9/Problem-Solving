f73a98470bed1db315593623fc6a767334c3aafe
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-05-23 05:21:27 +0530

    Time: 263 ms (36.96%), Space: 79.5 MB (25.54%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -3,33 +3,36 @@ private:
     bool isSafe(int row, int col, int m, int n) {
         return (row >= 0) && (row < m) && (col >= 0) && (col < n);
     }
-    void dfs(vector<vector<int> >& land, int i, int j, pair<int, int>& p, int m, int n) {
+    void dfs(vector<vector<int> >& land, int i, int j, pair<int, int>& p,int& zeroCnt, int m, int n) {
         for(int k = 0; k < 4; k++) {
             if(isSafe(i + vp[k].first, j + vp[k].second, m, n) && land[i + vp[k].first][j + vp[k].second]) {
+                zeroCnt++;
                 land[i + vp[k].first][j + vp[k].second] = 0;
                 if((i + j) < ((i + vp[k].first) + (j + vp[k].second))) {
                     p.first = i + vp[k].first;
                     p.second = j + vp[k].second;
                 }
-                dfs(land, i + vp[k].first, j + vp[k].second, p, m, n);
+                dfs(land, i + vp[k].first, j + vp[k].second, p, zeroCnt, m, n);
             }
         }
     }
 public:
     vector<pair<int, int> > vp = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        int m = land.size(), n = land[0].size();
+        int m = land.size(), n = land[0].size(), zeroCnt = 0;
         vector<vector<int> > v;
         
         for(int i = 0; i < m; i++) {
+            if(zeroCnt == m * n) break;
             for(int j = 0; j < n; j++) {
                 if(land[i][j]) {
+                    zeroCnt++;
                     land[i][j] = 0;
                     vector<int> tempVec;
                     tempVec.push_back(i), tempVec.push_back(j);
                     pair<int, int> p = {i, j};
                     
-                    dfs(land, i, j, p, m, n);
+                    dfs(land, i, j, p, zeroCnt, m, n);
                     
                     tempVec.push_back(p.first), tempVec.push_back(p.second);
                     v.push_back(tempVec);
