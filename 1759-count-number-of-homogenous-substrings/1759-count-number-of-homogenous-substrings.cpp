92eae9c51693e1a10ad9596b1022e731b17258e9
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-06-03 02:08:41 +0530

    Time: 31 ms (55.34%), Space: 11.6 MB (77.67%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -3,12 +3,15 @@ const int mod = 1e9 + 7;
 class Solution {
 public:
     int countHomogenous(string s) {
-        int res(0);
-        for(long long int i = 1, j = 0; i <= (int)s.size(); i++) if(i == s.size() || s[i - 1] != s[i]) res = (res + (((i - j) * (i - j + 1)) / 2)) % mod, j = i;
+        int res(0), cnt(0), prev(0);
+        for(int curr : s) {
+            cnt = (prev == curr) ? ++cnt : 1;
+            prev = curr;
+            res = (res + cnt) % mod;
+        }
         return res;
     }
 };
-
 
 /*
 
@@ -27,17 +30,14 @@ int countHomogenous(string s) {
 
 /*
 
+// Sliding Window
 const int mod = 1e9 + 7;
 
 class Solution {
 public:
     int countHomogenous(string s) {
-        int res(0), cnt(0), prev(0);
-        for(int curr : s) {
-            cnt = (prev == curr) ? ++cnt : 1;
-            prev = curr;
-            res = (res + cnt) % mod;
-        }
+        int res(0);
+        for(long long int i = 1, j = 0; i <= (int)s.size(); i++) if(i == s.size() || s[i - 1] != s[i]) res = (res + (((i - j) * (i - j + 1)) / 2)) % mod, j = i;
         return res;
     }
 };
