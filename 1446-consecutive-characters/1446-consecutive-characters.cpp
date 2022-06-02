e8f2cf1ad4123356ae8197c7b89688ac2010d6d3
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-06-03 00:41:01 +0530

    Time: 4 ms (58.82%), Space: 6.8 MB (27.24%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int maxPower(string s) {
-        if(s.size() == 1) return 1;
         int maxi = 1, cnt = 1;
         char prevChar = s[0];
-        for(int i = 1; i < (int)s.size(); i++) {
-            char currChar = s[i];
-            if(prevChar == currChar) {
+        for(int i = 1; s[i]; i++) {
+            if(prevChar == s[i]) {
                 cnt++;
                 maxi = max(maxi, cnt);
                 continue;
             }
             cnt = 1;
-            prevChar = currChar;
+            prevChar = s[i];
         }
         return maxi;
     }
