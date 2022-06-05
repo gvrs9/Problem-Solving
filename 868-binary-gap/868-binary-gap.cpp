98cd9fa2c8dca3076d01ad240504c596847b5c29
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-06-05 06:26:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.46%) - LeetHub

--- 868-binary-gap/868-binary-gap.cpp
+++ 868-binary-gap/868-binary-gap.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int binaryGap(int n) {
+        int j = -1, maxiDistance = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n >> i) & 1) {
+                if (j != -1) maxiDistance = max(maxiDistance, i - j);
+                j = i;
+            }
+        }
+        return maxiDistance;
+    }
+};
+
+/*
+class Solution {
+public:
+    int binaryGap(int n) {
         int logNum = (int)(log2(n));
         string s = bitset<32>(n).to_string().substr(32 - (int)(log2(n)) - 1);
         int maxi = INT_MIN;
@@ -8,3 +23,4 @@ public:
         return maxi;
     }
 };
+*/
