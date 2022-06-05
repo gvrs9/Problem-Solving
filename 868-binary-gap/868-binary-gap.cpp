1c921042e66e786b6b474216ac3544e2d9ad7fa9
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-06-05 05:44:47 +0530

    Time: 4 ms (25.22%), Space: 5.9 MB (42.93%) - LeetHub

--- 868-binary-gap/868-binary-gap.cpp
+++ 868-binary-gap/868-binary-gap.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int binaryGap(int n) {
         int logNum = (int)(log2(n));
-        string s = bitset<32>(n).to_string().substr(32 - logNum - 1);
+        string s = bitset<64>(n).to_string().substr(64 - logNum - 1);
         int maxi = INT_MIN;
         for(int i = 0, j = 0; i < (int)s.size(); i++) if(s[i] == '1') maxi = max(maxi, i - j), j = i;
         return maxi;
