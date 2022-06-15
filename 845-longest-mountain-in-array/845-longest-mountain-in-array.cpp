4f8929df7c30911930d88b5bcd6ac1cc8a808da6
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-06-15 21:11:55 +0530

    Time: 12 ms (98.85%), Space: 18.5 MB (83.33%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -7,7 +7,6 @@ public:
         int n = arr.size();
         while(i < n) {
             while(i < n && up == 0 && (arr[i - 1] > arr[i] || arr[i - 1] == arr[i])) i++;
-            while(i < n && arr[i - 1] == arr[i]) i++;
             int up = 0, down = 0;
             while(i < n && arr[i - 1] < arr[i]) up++, i++;
             while(up != 0 && i < n && arr[i - 1] > arr[i]) down++, longestLen = max(longestLen, up + down + 1), i++;
