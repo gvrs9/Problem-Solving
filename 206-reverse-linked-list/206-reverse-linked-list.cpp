416a5c6abcb0ec1a88e0eac2c011f9a10f48db94
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-07-20 12:09:30 +0530

    Time: 7 ms (79.41%), Space: 8.6 MB (10.52%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -9,10 +9,35 @@
  * };
  */
 class Solution {
+private:
+    void reverse(ListNode* &head, ListNode* curr, ListNode* prev) {
+        if(curr == __null) {
+            head = prev;
+            return;
+        }
+        
+        ListNode* forward = curr -> next;
+        reverse(head, forward, curr);
+        curr -> next = prev;
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        // Recursive Approach
+        if(head == __null || head -> next == __null) return head;
+        ListNode* prev = __null;
+        ListNode* curr = head;
+        
+        reverse(head, curr, prev);
+        return head;
+    }
+};
+
+/*
+class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         // Iterative Approach
-        if(head == __null || head -> next == __null) return head;
+        
         ListNode* prev = __null;
         ListNode* curr = head;
         ListNode* forward = __null;
@@ -26,3 +51,4 @@ public:
         return prev;
     }
 };
+*/
