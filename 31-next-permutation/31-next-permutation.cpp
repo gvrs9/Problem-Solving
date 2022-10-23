ef85c58b406451b18881ffd945ef7cb4a6d45be1
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-10-23 07:15:59 +0530

    Time: 19 ms (6.76%), Space: 12.1 MB (36.72%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums, int low, int high) {
+        while(low < high) swap(nums[low++], nums[high--]);
+    }
+    void nextPermutation(vector<int>& nums) {
+        // [2,1,4,7,10] --> [2,1,4,10,7], [2,1,7,4,10], [2,1,7,10,4], [2,1,10,4,7], [2,1,10,7,4], [2,4,1,7,10], [2,4,1,10,7], etc.
+        if(nums.size() <= 1) return;
+        int breakPoint = nums.size() - 2;
+        while(breakPoint >= 0 && nums[breakPoint + 1] <= nums[breakPoint]) breakPoint--; // figuring out breakPoint
+        // cout << breakPoint;
+        if(breakPoint >= 0) {
+            int j = nums.size() - 1;
+            while(j >= 0 && nums[j] <= nums[breakPoint]) j--;
+            swap(nums[breakPoint], nums[j]);
+        }
+        reverse(nums, breakPoint + 1, nums.size() - 1);
+    }
+};
