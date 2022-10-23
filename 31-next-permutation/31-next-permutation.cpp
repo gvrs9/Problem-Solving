55c532dc4662b0a28ca2926c556ea100d2ada930
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-10-23 07:28:04 +0530

    Time: 7 ms (64.31%), Space: 11.9 MB (93.21%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -7,7 +7,7 @@ public:
         // [2,1,4,7,10] --> [2,1,4,10,7], [2,1,7,4,10], [2,1,7,10,4], [2,1,10,4,7], [2,1,10,7,4], [2,4,1,7,10], [2,4,1,10,7], etc.
         if(nums.size() <= 1) return;
         int breakPoint = nums.size() - 2;
-        while(breakPoint >= 0 && nums[breakPoint + 1] <= nums[breakPoint]) breakPoint--; // figuring out breakPoint
+        while(breakPoint >= 0 && (nums[breakPoint + 1] <= nums[breakPoint])) breakPoint--; // figuring out breakPoint
         // cout << breakPoint;
         if(breakPoint >= 0) {
             int j = nums.size() - 1;
