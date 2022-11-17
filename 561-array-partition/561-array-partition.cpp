cd7ee108a23c3cd609a50e22ce4fcb81e0bc309a
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-18 01:14:49 +0530

    Time: 116 ms (49.91%), Space: 28.4 MB (49.32%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -3,15 +3,15 @@ public:
     int arrayPairSum(vector<int>& nums) {
         int hsh[20001] = {0};
         for(int i = 0; i < (int)nums.size(); i++) hsh[nums[i] + 10000]++;
-        int maxiSum = 0;
+        int maxi = 0;
         bool flag = true;
         for(int i = 0; i < 20001; i++) {
             while(hsh[i]) {
-                if(flag) maxiSum += i - 10000;
+                if(flag) maxi += i - 10000;
                 flag = !flag;
                 hsh[i]--;
             }
         }
-        return maxiSum;
+        return maxi;
     }
 };
