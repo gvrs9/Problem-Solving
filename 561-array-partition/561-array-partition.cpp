3a4beea5db68edde2c66f9db68830f447343d6bd
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-18 01:13:04 +0530

    Time: 138 ms (20.22%), Space: 28.3 MB (49.32%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -4,11 +4,11 @@ public:
         int hsh[20001] = {0};
         for(int x : nums) hsh[x + 10000]++;
         int maxiSum = 0;
-        bool odd = true;
+        bool flag = true;
         for(int i = 0; i < 20001; i++) {
             while(hsh[i]) {
-                if(odd) maxiSum += i - 10000;
-                odd = !odd;
+                if(flag) maxiSum += i - 10000;
+                flag = !flag;
                 hsh[i]--;
             }
         }
