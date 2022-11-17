3256cb43c2e8802666de9e47ba0bc9e5a3b8d106
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-18 01:01:35 +0530

    Time: 74 ms (75.31%), Space: 28.3 MB (5.25%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        int max = *max_element(nums.begin(), nums.end());
-        int min = *min_element(nums.begin(), nums.end());
-        int k = max - min + 1;
-        int n = nums.size();
-        vector<int> count(k), output(n);
-        for(int x : nums) count[x - min]++;
-        for(int i = 1; i < k; i++) count[i] += count[i - 1];
-        for(int i = n - 1; i >= 0; i--) {
-            output[count[nums[i] - min] - 1] = nums[i];
-            count[nums[i] - min]--;
+        int hsh[20001] = {0};
+        for(int x : nums) hsh[x + 10000]++;
+        int maxiSum = 0;
+        bool odd = true;
+        for(int i = 0; i < 20001; i++) {
+            while(hsh[i]) {
+                if(odd) maxiSum += i - 10000;
+                odd = !odd;
+                hsh[i]--;
+            }
         }
-        int maxi = 0;
-        for(int i = 0; i < (int)output.size(); i += 2) maxi += output[i];
-        return maxi;
+        return maxiSum;
     }
 };
