310eaffa7b052835d57eb2b824c0547da71bdf3c
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-21 02:42:57 +0530

    Time: 48 ms (7.03%), Space: 9.5 MB (16.51%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        // if stuck, refer to this NeetCode's video -> "https://www.youtube.com/watch?v=ohBNdSJyLh8"
         string res = "";
         vector<pair<string, int> > vp = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}};
         for(auto it = vp.rbegin(); it != vp.rend(); it++) { // vp is reverse iterated!!
