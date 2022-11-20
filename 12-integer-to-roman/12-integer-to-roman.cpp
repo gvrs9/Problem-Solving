1134f53e5159511ecb3cc358e3ce5c6b271402c8
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-21 02:34:32 +0530

    Time: 25 ms (26.18%), Space: 9.4 MB (19.17%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -3,11 +3,12 @@ public:
     string intToRoman(int num) {
         string res = "";
         vector<pair<string, int> > vp = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}};
-        for(auto it = vp.rbegin(); it != vp.rend(); it++) {
-            if(num / it -> second) {
+        for(auto it = vp.rbegin(); it != vp.rend(); it++) { // vp is reverse iterated!!
+            if(num / it -> second) { // number "num" is being forward iterated!!
                 int cnt = num / it -> second;
                 while(cnt--) res += it -> first;
-                num %= it -> second;
+                cout << res << "\n";
+                num %= it -> second; // ex: num = 3529. So, 3529 % 1000 = 529. Similarly, 529 % 500 = 29. //ly 29 % 10 = 9
             }
         }
         return res;
