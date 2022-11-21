5ea2943876d56de8e702683d6a41a07a6ea512bb
GVRS sai Ganesh <63167616+gvrs9@users.noreply.github.com>
2022-11-22 02:57:30 +0530

    Time: 3 ms (65.67%), Space: 5.7 MB (94.06%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -11,11 +11,22 @@ private:
     }
 public:
     bool isHappy(int n) {
+        /*
+        // O(log n) -> t.c and s.c
         unordered_set<int> s;
         while(n != 1 && s.find(n) == s.end()) {
             s.insert(n);
             n = squareDigitSum(n);
         }
         return n == 1;
+        */
+        
+        // O(log n) -> T.C and O(1) -> S.C using slow-fast pointer method!!
+        int slowRunner = n, fastRunner = squareDigitSum(n);
+        while(fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = squareDigitSum(slowRunner); // once at a time
+            fastRunner = squareDigitSum(squareDigitSum(fastRunner)); // twice speed
+        }
+        return fastRunner == 1;
     }
 };
