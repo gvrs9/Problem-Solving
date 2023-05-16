146a2c74767a520d0f15a59843632ae52e6a22ae
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-05-16 07:29:12 -0700

    Time: 4 ms (84.75%), Space: 11.5 MB (85.86%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < (int)nums.size(); i++) if(nums[i] == target) ans.push_back(i);
+        return ans;
+    }
+};
