4b41b68ae86695589a1837626c916bb1a4541187
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-07-27 14:01:55 +0530

    Time: 4 ms (63.52%), Space: 41.3 MB (49.17%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -3,7 +3,6 @@ class Solution {
         int[] hash = new int[26];
         Arrays.fill(hash, 0);
         for(int i = 0; i < text.length(); i++) hash[text.charAt(i) - 'a']++;
-        // for(char x : text) hash[x - 'a']++;
         int banMin = Math.min(Math.min(hash['b' - 'a'], hash['a' - 'a']), hash['n' - 'a']);
         if(banMin < 1) return 0;
         int loMin = Math.min(hash['l' - 'a'], hash['o' - 'a']) / 2;
