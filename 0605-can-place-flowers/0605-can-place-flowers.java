341fc84ecf4b69b687ebd043a913770051d6ac82
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-07-27 09:10:37 +0530

    Time: 1 ms (99.29%), Space: 44.9 MB (7.76%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -8,19 +8,15 @@ class Solution {
         }
         for(int i = 0; i < flowerbed.length; i++) {
             if(n > 0 && flowerbed[i] == 0) {
-                // System.out.println(i + " " + n);
                 if(i - 1 >= 0 && flowerbed[i - 1] == 0 && i + 1 < flowerbed.length && flowerbed[i + 1] == 0) {
                     flowerbed[i] = 1;
                     n--;
-                    System.out.println(i + " " + n + " " + flowerbed[i]);
                 }else if(i - 1 < 0 && flowerbed[i + 1] == 0) {
                     flowerbed[i] = 1;
                     n--;
-                    System.out.println(i + " " + n + " " + flowerbed[i]);
                 }else if(i + 1 == flowerbed.length && flowerbed[i - 1] == 0) {
                     flowerbed[i] = 1;
                     n--;
-                    System.out.println(i + " " + n + " " + flowerbed[i]);
                 }
             }else if(n == 0) {
                 return true;
