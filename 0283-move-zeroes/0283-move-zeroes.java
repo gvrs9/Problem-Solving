896e4a97bca47adcdba1889a18acee63fe2f019c
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-08-17 13:22:33 +0530

    Time: 2 ms (50.03%), Space: 45 MB (71.50%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,9 +5,6 @@ class Solution {
         nums[y] = temp;
     }
     public void moveZeroes(int[] nums) {
-        int k = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) swap(k++, i, nums);
-        }
+        for(int i = 0, k = 0; i < nums.length; i++) if(nums[i] != 0) swap(k++, i, nums);
     }
 }
