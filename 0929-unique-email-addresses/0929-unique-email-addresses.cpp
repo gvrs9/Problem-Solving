e5c7545a47b9a37cba789d7febfe3b93e01cbbc0
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-18 08:18:01 +0530

    Time: 23 ms (55.52%), Space: 14 MB (64.84%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.cpp
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -8,6 +8,38 @@ public:
             int n = emails[i].size();
             int j = 0;
             
+            while(j < n) {
+                if(emails[i][j] == '+' || emails[i][j] == '@') break;
+                if(emails[i][j] == '.') {
+                    j++;
+                    continue;
+                }
+                str += emails[i][j++];
+            }
+            
+            while(emails[i][j] != '@') j++;
+            
+            while(j < n) str += emails[i][j++];
+            
+            st.insert(str);
+        }
+        return st.size();
+    }
+};
+
+
+/*
+
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_set<string> st;
+        
+        for(int i = 0; i < (int)emails.size(); i++) {
+            string str = "";
+            int n = emails[i].size();
+            int j = 0;
+            
             while(emails[i][j] != '+' && emails[i][j] != '@') {
                 // if(emails[i][j] == '+' || emails[i][j] == '@') break;
                 if(emails[i][j] == '.') {
@@ -27,3 +59,5 @@ public:
         return st.size();
     }
 };
+
+*/
