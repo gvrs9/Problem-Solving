58b3a6918b74fe54948fd6237b12882c0445d8b2
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-18 06:21:43 +0530

    Time: 7 ms (53.42%), Space: 7.3 MB (9.65%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int hsh_Map_S_To_T[256] = {0}, hsh_Map_T_To_S[256] = {0};
+        int hsh_S_To_T[256] = {0};
+        bool T_hsh[256] = {false};
         
         for(int i = 0; i < (int)s.size(); i++) {
-            if((hsh_Map_S_To_T[s[i]] && hsh_Map_S_To_T[s[i]] != t[i]) || (hsh_Map_T_To_S[t[i]] && hsh_Map_T_To_S[t[i]] != s[i])) return false;
-            hsh_Map_S_To_T[s[i]] = t[i];
-            hsh_Map_T_To_S[t[i]] = s[i];
+            char sCurrChar = s[i], tCurrChar = t[i];
+            
+            if(hsh_S_To_T[sCurrChar]) {
+                if(!T_hsh[tCurrChar] || hsh_S_To_T[sCurrChar] != tCurrChar) return false;
+            }else {
+                cout << sCurrChar << " " << tCurrChar << "\n";
+                if(T_hsh[tCurrChar]) return false;
+                hsh_S_To_T[sCurrChar] = tCurrChar;
+                T_hsh[tCurrChar] = true;
+            }
         }
-        
+        cout << "______________________\n";
         return true;
     }
 };
