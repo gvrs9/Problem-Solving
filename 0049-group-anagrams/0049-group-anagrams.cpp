8c6db2a0b031e3213dacf6aac76423b966aca18c
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-19 12:11:13 +0530

    Time: 40 ms (27.87%), Space: 24.2 MB (15.00%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,6 +4,28 @@ public:
         unordered_map<string, vector<string>> umap;
         for(string str : strs) {
             string tempStr = str;
+            int chFreq[26] = {0};
+            for(char ch : str) chFreq[ch - 'a']++;
+            string keyString;
+            for(int x : chFreq) keyString += (x + '0');
+            umap[keyString].push_back(str);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto [x, y]: umap) ans.push_back(y);
+        
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Time complexity: O(nmlogm)
+        unordered_map<string, vector<string>> umap;
+        for(string str : strs) {
+            string tempStr = str;
             sort(tempStr.begin(), tempStr.end());
             umap[tempStr].push_back(str);
         }
@@ -14,3 +36,4 @@ public:
         return ans;
     }
 };
+*/
