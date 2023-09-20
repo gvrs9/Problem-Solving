094179b64d235a83af7407914afe5e3d219c577e
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-21 04:00:00 +0530

    Time: 3 ms (88.04%), Space: 9.1 MB (20.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,11 +4,33 @@ public:
         vector<int> ans(nums1.size(), -1);
         unordered_map<int, int> umap;
         
-        for(int i = 0; i < (int)nums2.size(); i++) umap[nums2[i]] = i;
+        for(int i = 0; i < (int)nums1.size(); i++) umap[nums1[i]] = i;
+        for(int i = 0; i < (int)nums2.size(); i++) {
+            if(umap.find(nums2[i]) == umap.end()) continue;
+            for(int j = i; j < (int)nums2.size(); j++) {
+                if(nums2[i] < nums2[j]) {
+                    ans[umap[nums2[i]]] = nums2[j];
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // Brute Force!! O(n*m) -> T.C && space complexity of O(m) where n and m nums1.size() and nums2.size()
+        vector<int> ans(nums1.size(), -1);
+        unordered_map<int, int> umap;
+        
+        for(int i = 0; i < (int)nums2.size(); i++) umap[nums2[i]] = i; // {value, index} map!!
         for(int i = 0; i < (int)nums1.size(); i++) {
-            // int x = umap[nums1[i]];
             int pos = umap[nums1[i]];
-            
+            pos += 1; // we don't need to check itself!!
             while(pos < (int)nums2.size()) {
                 if(nums1[i] < nums2[pos]) {
                     ans[i] = nums2[pos];
@@ -16,8 +38,10 @@ public:
                 }
                 pos++;
             }
+            
         }
         
         return ans;
     }
 };
+*/
