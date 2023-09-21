45f8a9156deb7d27228ac9550fcd7d6183c83497
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-21 09:47:32 +0530

    Update next-greater-element-basic.cpp

--- next-greater-element-basic.cpp
+++ next-greater-element-basic.cpp
@@ -1,7 +1,24 @@
 #include "iostream"
 #include "vector"
 #include "stack"
+#include <stack>
 using namespace std;
+
+vector<int> next_greater_element_other_method(int arr[], int size) {
+	vector<int> ans(size, -1);
+	stack<int> stk;
+
+	// Traversing the array from reverse
+	for (int i = size - 1; i >= 0; i--) {
+		int currElement = arr[i];
+		while (!stk.empty() && currElement >= arr[stk.top()]) stk.pop(); // Consider having >= or just > based on whether the given array has unique elements or repeated elements. For unique, we can just keep >.
+		int res = stk.empty() ? -1 : arr[stk.top()];
+		ans[i] = res;
+		stk.push(i);
+	}
+
+	return ans;
+}
 
 vector<int> next_greater_element(int arr[], int size) {
 	vector<int> ans(size, -1);
@@ -9,7 +26,7 @@ vector<int> next_greater_element(int arr[], int size) {
 
 	for (int i = 0; i < size; i++) {
 		int currElement = arr[i];
-		while (!stk.empty() && currElement > arr[stk.top()]) {
+		while (!stk.empty() && currElement >= arr[stk.top()]) { // Consider having >= or just > based on whether the given array has unique elements or repeated elements. For unique, we can just keep >.
 			int index = stk.top();
 			stk.pop();
 			ans[index] = currElement;
@@ -21,15 +38,54 @@ vector<int> next_greater_element(int arr[], int size) {
 }
 
 int main() {
-	int size;
-	cin >> size;
+	int t;
+	cin >> t;
 
-	int arr[size];
-	for (int& x : arr) cin >> x;
+	/*
+		Sample I/P:
+			4
+			8
+			4 3 2 1 5 7 0 6
+			5
+			4 5 2 3 1
+			5
+			1000 1 2 3 7
+			11
+			10 100 25 27 200 3 5 4 1 36 475
 
-	vector<int> ans = next_greater_element(arr, size);
+		Sample O/P:
+			5 5 5 5 7 -1 6 -1
+			5 5 5 5 7 -1 6 -1
+			_____________________________
+			5 -1 3 -1 -1
+			5 -1 3 -1 -1
+			_____________________________
+			-1 2 3 7 -1
+			-1 2 3 7 -1
+			_____________________________
+			100 200 27 200 475 5 36 36 36 475 -1
+			100 200 27 200 475 5 36 36 36 475 -1
+			_____________________________
 
-	for (int x : ans) cout << x << " ";
+	*/
+
+	while (t--) {
+		int size;
+		cin >> size;
+
+		int arr[size];
+		for (int& x : arr) cin >> x;
+
+		vector<int> ans1 = next_greater_element(arr, size);
+		vector<int> ans2 = next_greater_element_other_method(arr, size);
+
+		for (int x : ans1) cout << x << " ";
+		cout << "\n";
+		for (int x : ans2) cout << x << " ";
+
+		cout << "\n";
+		cout << "_____________________________\n";
+	}
 
 	return 0;
 }
