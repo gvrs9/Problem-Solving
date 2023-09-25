6976dd7a33f3985320851a6506f017944c33ecc0
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 04:56:06 +0530

    Time: 0 ms (100.00%), Space: 7 MB (9.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    char findTheDifference(string s, string t) {
-        char ch = 0;
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         char ch = 0;
         
-        // 0 ^ ch -> ch ^ 0 -> ch. ch ^ ch -> 0.
+//         // 0 ^ ch -> ch ^ 0 -> ch. ch ^ ch -> 0.
         
-        // Ex: s = abc and t = bcax. Applying xor on s, a ^ b ^ c and similarly on t, b ^ c ^ a ^ x same as a ^ b ^ c ^ x. And xor both the strings s and t --> (a ^ b ^ c) ^ (a ^ b ^ c ^ x) --> (a ^ a ^ b ^ b ^ c ^ c ^ x) --> (0 ^ x) --> x.
+//         // Ex: s = abc and t = bcax. Applying xor on s, a ^ b ^ c and similarly on t, b ^ c ^ a ^ x same as a ^ b ^ c ^ x. And xor both the strings s and t --> (a ^ b ^ c) ^ (a ^ b ^ c ^ x) --> (a ^ a ^ b ^ b ^ c ^ c ^ x) --> (0 ^ x) --> x.
         
-        for(char x : s) ch ^= x;
+//         for(char x : s) ch ^= x;
         
-        for(char x : t) ch ^= x;
+//         for(char x : t) ch ^= x;
         
-        return ch;
-    }
-};
+//         return ch;
+//     }
+// };
 
 
 /*
@@ -30,3 +30,19 @@ public:
     }
 };
 */
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ch = 0;
+        int i = 0;
+        
+        while(i < (int)s.size()) ch += s[i], ch -= t[i++];
+        
+        ch -= t[i];
+        // cout << int(ch) << "\n";
+        ch *= -1;
+        
+        return ch;
+    }
+};
