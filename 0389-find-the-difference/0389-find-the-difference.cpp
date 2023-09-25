deef52ce65aeca3d969951ed2e16b594247e5f31
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 04:22:32 +0530

    Time: 2 ms (70.45%), Space: 6.9 MB (17.70%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int i = 0, sSum = 0, tSum = 0;
-        while(i < (int)s.size()) sSum += (s[i] - 'a'), tSum += (t[i++] - 'a');
-        tSum += (t[i] - 'a');
         
-        return char((tSum - sSum) + 97);
+        while(i < (int)s.size()) sSum += s[i], tSum += t[i++];
+        
+        tSum += t[i];
+        
+        return char(tSum - sSum); // don't need to explicitly convert it.
     }
 };
