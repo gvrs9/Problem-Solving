3b69d434e1ae0901ca334235f6111a8e0475fd5a
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 05:00:48 +0530

    Time: 4 ms (47.89%), Space: 7.2 MB (5.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,25 +1,28 @@
-// class Solution {
-// public:
-//     char findTheDifference(string s, string t) {
-//         char ch = 0;
+/*
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ch = 0;
         
-//         // 0 ^ ch -> ch ^ 0 -> ch. ch ^ ch -> 0.
+        // 0 ^ ch -> ch ^ 0 -> ch. ch ^ ch -> 0.
         
-//         // Ex: s = abc and t = bcax. Applying xor on s, a ^ b ^ c and similarly on t, b ^ c ^ a ^ x same as a ^ b ^ c ^ x. And xor both the strings s and t --> (a ^ b ^ c) ^ (a ^ b ^ c ^ x) --> (a ^ a ^ b ^ b ^ c ^ c ^ x) --> (0 ^ x) --> x.
+        // Ex: s = abc and t = bcax. Applying xor on s, a ^ b ^ c and similarly on t, b ^ c ^ a ^ x same as a ^ b ^ c ^ x. And xor both the strings s and t --> (a ^ b ^ c) ^ (a ^ b ^ c ^ x) --> (a ^ a ^ b ^ b ^ c ^ c ^ x) --> (0 ^ x) --> x.
         
-//         for(char x : s) ch ^= x;
+        for(char x : s) ch ^= x;
         
-//         for(char x : t) ch ^= x;
+        for(char x : t) ch ^= x;
         
-//         return ch;
-//     }
-// };
+        return ch;
+    }
+};
+*/
 
 
 /*
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        // Using two variables approach.
         int i = 0, sSum = 0, tSum = 0;
         
         while(i < (int)s.size()) sSum += s[i], tSum += t[i++];
@@ -31,13 +34,14 @@ public:
 };
 */
 
+
 /*
 class Solution {
 public:
     char findTheDifference(string s, string t) {
         char ch = 0;
         int i = 0;
-        
+        // Single variable approach.
         while(i < (int)s.size()) ch += s[i], ch -= t[i++];
         
         ch -= t[i];
@@ -49,6 +53,7 @@ public:
 };
 */
 
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -58,7 +63,6 @@ public:
         while(i < (int)s.size()) ch -= s[i], ch += t[i++];
         
         ch += t[i];
-        // cout << int(ch) << "\n";
         
         return ch;
     }
