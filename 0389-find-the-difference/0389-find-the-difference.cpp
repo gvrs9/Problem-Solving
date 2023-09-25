e006f8f5e19462c7dd85c551560b6091187f98be
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 04:27:51 +0530

    Time: 0 ms (100.00%), Space: 7 MB (9.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        char ch = 0;
+        
+        for(char x : s) ch ^= x;
+        for(char x : t) ch ^= x;
+        
+        return ch;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
         int i = 0, sSum = 0, tSum = 0;
         
         while(i < (int)s.size()) sSum += s[i], tSum += t[i++];
@@ -10,3 +24,4 @@ public:
         return char(tSum - sSum); // don't need to explicitly convert it.
     }
 };
+*/
