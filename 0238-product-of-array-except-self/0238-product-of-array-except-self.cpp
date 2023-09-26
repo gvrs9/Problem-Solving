6814f650a8d0052eba27f0c26d80fc64ae603046
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 12:24:13 +0530

    Time: 20 ms (41.88%), Space: 24.4 MB (54.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // We use only the output array for first calculating the prefix product and then to calculate te the suffix product and simultaneously the prefix * suffix product.
+        // O(n) -> T.C & S.C -> O(1)
+        
+        vector<int> ans(nums.size());
+        ans[0] = 1;
+        // calculating the prefix product
+        for(int i = 1; i < (int)nums.size(); i++) ans[i] = ans[i - 1] * nums[i - 1];
+        // So for [1,2,3,4], the ans will now look like [1,1,2,6]. We don't need to store for the 4th ele as we don't require it and for it, the suffix product is 1.
+        
+        // we now calculate the suffix product and simultaneously calculate the prefix * suffix product. 
+        // Take suffix product variable "suffix" and set it to 1. Since we k/t, for the (n - 1)th ele of nums[], suffix product is 1 as there is no ele after it.
+        
+        int suffix = 1;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            int prefix = ans[i];
+            ans[i] = prefix * suffix; // store the product in the same position.
+            suffix *= nums[i]; // update the suffix.
+        }
+        
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
         // prefix sum using prefix array and suffix(or postfix) array
         // O(n) -> T.C & S.C
         
@@ -26,6 +53,8 @@ public:
         return ans;
     }
 };
+*/
+
 
 /*
 class Solution {
