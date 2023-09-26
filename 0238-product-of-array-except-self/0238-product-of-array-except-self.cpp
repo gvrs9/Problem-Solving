45023daed78d8ac1661b2a18a8349b8c4fbaab2c
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 10:58:17 +0530

    Time: 19 ms (54.27%), Space: 25.4 MB (15.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -10,17 +10,17 @@ public:
         
         for(int i = 1; i < (int)pref.size(); i++) pref[i] = pref[i - 1] * nums[i - 1];
         
-        for(int x : pref) cout << x << " ";
-        cout << "\n";
+        // for(int x : pref) cout << x << " ";
+        // cout << "\n";
         
         for(int i = suff.size() - 2; i >= 0; i--) suff[i] = suff[i + 1] * nums[i];
         
-        for(auto x : suff) cout << x << " ";
-        cout << "\n";
+        // for(auto x : suff) cout << x << " ";
+        // cout << "\n";
         
         for(int i = 0; i < (int)nums.size(); i++) ans[i] = pref[i] * suff[i + 1];
         
-        cout << "----------------------------------------------------------------------\n";
+        // cout << "----------------------------------------------------------------------\n";
         
         return ans;
     }
