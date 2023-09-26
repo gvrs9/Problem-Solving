fdc74a863529094855e5e38ef5b3c74e2bff16d8
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-26 10:57:53 +0530

    Time: 87 ms (6.05%), Space: 25.5 MB (12.96%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // prefix sum using prefix array and suffix(or postfix) array
+        
+        vector<int> ans(nums.size()), pref(nums.size() + 1), suff(nums.size() + 1);
+        
+        pref[0] = 1;
+        suff[nums.size()] = 1;
+        
+        for(int i = 1; i < (int)pref.size(); i++) pref[i] = pref[i - 1] * nums[i - 1];
+        
+        for(int x : pref) cout << x << " ";
+        cout << "\n";
+        
+        for(int i = suff.size() - 2; i >= 0; i--) suff[i] = suff[i + 1] * nums[i];
+        
+        for(auto x : suff) cout << x << " ";
+        cout << "\n";
+        
+        for(int i = 0; i < (int)nums.size(); i++) ans[i] = pref[i] * suff[i + 1];
+        
+        cout << "----------------------------------------------------------------------\n";
+        
+        return ans;
+    }
+};
