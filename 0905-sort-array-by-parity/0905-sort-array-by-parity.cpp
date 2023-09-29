c1274e88e5ef6f3ffe64fd33da600892117cdbc3
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 04:33:29 +0530

    Time: 7 ms (65.29%), Space: 16.5 MB (50.69%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0, right = 0;
         
         while(right < (int)nums.size()) {
-            if(!(nums[right] & 1)) swap(nums[left++], nums[right]);
+            if(nums[right] % 2 == 0) swap(nums[left++], nums[right]);
             right++;
         }
         
