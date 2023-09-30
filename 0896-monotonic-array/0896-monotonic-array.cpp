4e2072c92c1e5eecb886d336d4d82867466e68b5
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 05:49:46 +0530

    Time: 143 ms (7.79%), Space: 97 MB (9.26%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -4,11 +4,11 @@ public:
         bool increasing = false, decreasing = false;
         
         for(int i = 1; i < (int)nums.size(); i++) {
-            if(increasing && decreasing) return false;
             if(nums[i] < nums[i - 1]) decreasing = true;
             if(nums[i] > nums[i - 1]) increasing = true;
+            if(increasing && decreasing) return false;
         }
         
-        return (increasing && decreasing) ? false : true;
+        return true;
     }
 };
