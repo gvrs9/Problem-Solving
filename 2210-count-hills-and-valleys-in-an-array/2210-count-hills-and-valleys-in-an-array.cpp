91b7d35e11c14b5efbb2e0422945bc7ed5741dfa
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 10:07:19 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (19.46%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
@@ -1,18 +1,25 @@
-class Solution {
+/*class Solution {
 public:
     int countHillValley(vector<int>& nums) {
         // Storing index in the prevIndex.
         int cnt = 0, prevIndex = 0;
         
+        // [2,4,3,1,1,1,1,1,6,5,1,3,2]
+                             int temp = 0;
         for(int i = 1; i < (int)nums.size() - 1; i++) {
-            if((nums[i] > nums[prevIndex] && nums[i] > nums[i + 1]) || (nums[i] < nums[prevIndex] && nums[i] < nums[i + 1])) cnt++, prevIndex = i;
+            if((nums[i] > nums[prevIndex] && nums[i] > nums[i + 1]) || (nums[i] < nums[prevIndex] && nums[i] < nums[i + 1])) {
+                temp = prevIndex;
+                cnt++, prevIndex = i;
+                if(prevIndex == 7) cout << "temp --> " << temp << " " << "i --> " << i << " i + 1 --> " << i + 1 << " ";
+            }
+            cout << prevIndex << "\n";
         }
         
         return cnt;
     }
 };
+*/
 
-/*
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
@@ -20,10 +27,13 @@ public:
         int count = 0, prev = nums[0];
         
         for(int i = 1; i < (int)nums.size() - 1; i++) {
-            if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++, prev = nums[i];
-            // if(nums[i] != nums[i + 1]) prev = nums[i]; // We don't need this condition here. We can simply update the prev in the 1st condition as we only update prev when we are not looking at duplicates.
+            // if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++, prev = nums[i];
+            if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++;
+            if(nums[i] != nums[i + 1]) {
+                cout << nums[i] << "\n";
+                prev = nums[i]; // We don't need this condition here. We can simply update the prev in the 1st condition as we only update prev when we are not looking at duplicates.
+            }
         }
         return count;
     }
 };
-*/
