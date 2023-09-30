f3c838879b74218b18207fcf0caa90699f94db02
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 09:25:07 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (5.23%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
+        // Storing index in the prevIndex.
+        int cnt = 0, prevIndex = 0;
+        
+        for(int i = 1; i < (int)nums.size() - 1; i++) {
+            if((nums[i] > nums[prevIndex] && nums[i] > nums[i + 1]) || (nums[i] < nums[prevIndex] && nums[i] < nums[i + 1])) cnt++, prevIndex = i;
+        }
+        
+        return cnt;
+    }
+};
+
+/*
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        // Storing value in the prev.
         int count = 0, prev = nums[0];
         
         for(int i = 1; i < (int)nums.size() - 1; i++) {
             if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++, prev = nums[i];
-            // if(nums[i] != nums[i + 1]) prev = nums[i];
+            // if(nums[i] != nums[i + 1]) prev = nums[i]; // We don't need this condition here. We can simply update the prev in the 1st condition as we only update prev when we are not looking at duplicates.
         }
         return count;
     }
 };
+*/
