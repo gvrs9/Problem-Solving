b7e61c2d4a617d52ffc74b1e7f2ff12096a2feda
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 09:17:15 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (10.88%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0, prev = nums[0];
         
         for(int i = 1; i < (int)nums.size() - 1; i++) {
-            if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++;
-            if(nums[i] != nums[i + 1]) prev = nums[i];
+            if((nums[i] > prev && nums[i] > nums[i + 1]) || (nums[i] < prev && nums[i] < nums[i + 1])) count++, prev = nums[i];
+            // if(nums[i] != nums[i + 1]) prev = nums[i];
         }
         return count;
     }
