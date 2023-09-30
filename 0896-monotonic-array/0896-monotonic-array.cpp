951413eb4e4caa6a450cdfadd2552d3c7b047590
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-09-30 06:45:18 +0530

    Time: 122 ms (60.37%), Space: 96.7 MB (31.14%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        bool up = (nums[n - 1] - nums[0]) > 0;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] != nums[i - 1] && ((nums[i] - nums[i - 1]) > 0) != up) return false;
+        }
+
+        return true;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        // if(nums.size() == 1) return true;
+        
+        int n = nums.size();
+        bool up = (nums[0] < nums[n - 1]) > 0;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] != nums[i - 1] && (nums[i - 1] < nums[i]) != up) return false;
+        }
+
+        return true;
+    }
+};
+*/
+
+
+/*
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
         bool increasing = false, decreasing = false;
         
         for(int i = 1; i < (int)nums.size(); i++) {
@@ -12,3 +47,4 @@ public:
         return true;
     }
 };
+*/
