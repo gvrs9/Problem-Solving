7f7a2e6bd068a9448d53d5d4bc2b3d70fcd58d33
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-10-02 03:45:58 +0530

    Time: 12 ms (83.83%), Space: 10.2 MB (52.27%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,3 +1,11 @@
+auto init = []()
+{
+    ios::sync_with_stdio(false);  // Disable synchronization between C and C++ standard streams for performance
+    cin.tie(nullptr);             // Unlink cin from cout for performance reasons.
+    cout.tie(nullptr);            // Unlink cout from cin for performance reasons.
+    return 'c';                   // Return the character 'c'.
+}();
+
 class Solution {
 public:
     string reverseWords(string s) {
