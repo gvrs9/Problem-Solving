70c7d4b75ad5f381732d8662ef60a16da20f1352
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-10-02 03:48:58 +0530

    Time: 12 ms (83.83%), Space: 10.2 MB (52.27%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -15,8 +15,10 @@ public:
         for(int i = 0; i <= n; i++) {
             if(s[i] == ' ' || i == n) {
                 int mid = (wordStartIndex + (i)) / 2;
+                
                 for(int j = wordStartIndex; j < mid; j++) 
                     swap(s[j], s[(i - j - 1) + wordStartIndex]);
+                
                 wordStartIndex = i + 1;
             }
         }
