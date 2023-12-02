24620bfd3fd923a3922e53099584b30c0c0090e2
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-03 03:33:51 +0530

    Time: 39 ms (89.06%), Space: 18.2 MB (76.89%) - LeetHub

---
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        int hsh[26] = {0};
+        for(char &x : chars) hsh[x - 'a']++;
+        int totalSumOfLengths = 0;
+
+        for(int i = 0; i < (int)words.size(); i++) {
+            string word = words[i];
+            bool goodString = true;
+            int hshTemp[26];
+            copy(hsh, hsh + 26, hshTemp);
+
+            for(char &x : word) {
+                if(!hshTemp[x - 'a']) {
+                    goodString = false;
+                    break;
+                }
+                hshTemp[x - 'a']--;
+            }
+
+            if(goodString) totalSumOfLengths += word.size();
+        }
+
+        return totalSumOfLengths;
+    }
+};
