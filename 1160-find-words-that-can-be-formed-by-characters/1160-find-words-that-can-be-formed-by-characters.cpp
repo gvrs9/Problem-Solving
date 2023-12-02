1a00960fafba7865180c98ff7f9dcc90743fe541
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-03 03:40:10 +0530

    Time: 38 ms (91.68%), Space: 18 MB (79.35%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
+        /*
         int hsh[26] = {0};
         for(char &x : chars) hsh[x - 'a']++;
         int totalSumOfLengths = 0;
@@ -17,6 +18,28 @@ public:
                     break;
                 }
                 hshTemp[x - 'a']--;
+            }
+
+            if(goodString) totalSumOfLengths += word.size();
+        }
+
+        return totalSumOfLengths;
+        */
+        
+        int hsh[26] = {0};
+        for(char &x : chars) hsh[x - 'a']++;
+        int totalSumOfLengths = 0;
+
+        for(int i = 0; i < (int)words.size(); i++) {
+            string word = words[i];
+            bool goodString = true;
+            int hshTemp[26] = {0};
+
+            for(char &x : word) {
+                if(++hshTemp[x - 'a'] > hsh[x - 'a']) {
+                    goodString = false;
+                    break;
+                }
             }
 
             if(goodString) totalSumOfLengths += word.size();
