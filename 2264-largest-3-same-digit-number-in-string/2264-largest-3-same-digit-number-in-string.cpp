b2db788279f6bd45b7d9bc4c02471bbe78860376
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-05 02:40:30 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (59.41%) - LeetHub

---
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+        int maxi = -1;
+
+        for(int i = 1; i < (int)num.size() - 1; i++) {
+            if((num[i - 1] == num[i] && num[i] == num[i + 1]) && ((num[i] - '0') > maxi)) {
+                maxi = max(maxi, num[i] - '0');
+            }
+        }
+
+        if(maxi > -1) {
+            string temp = to_string(maxi);
+            ans += temp + temp + temp;
+        }
+
+        return ans;
+    }
+};
