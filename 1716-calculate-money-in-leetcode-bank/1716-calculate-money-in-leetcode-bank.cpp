cf0862bd8cd36180c904ab5321f64187fe338807
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-07 06:32:14 +0530

    Time: 3 ms (13.68%), Space: 6.3 MB (35.26%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -5,7 +5,7 @@ public:
         int sumUntil = ((((temp * (temp - 1)) / 2) * 7) + (28 * temp));
         int diff = (n - (temp * 7));
         if(!diff) return sumUntil;
-        int remaining = (temp * 7) + ((diff * (diff + 1)) / 2) - ((7 - diff) * temp);
+        int remaining = ((temp * 7) + ((diff * (diff + 1)) / 2) - ((7 - diff) * temp));
         return sumUntil + remaining;
     }
 };
