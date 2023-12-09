72678246be99719cc27558c91a445b8c22fc397a
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-09 05:50:13 +0530

    Time: 12 ms (73.23%), Space: 23.9 MB (80.92%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -17,11 +17,9 @@ private:
         ans += to_string(root -> val);
         if(!root -> right && !root -> left) return;
         
-        // if(root -> left) {
-            ans += "(";
-            treeToString(root -> left);
-            ans += ")";
-        // }
+        ans += "(";
+        treeToString(root -> left);
+        ans += ")";
         
         if(root -> right) {
             ans += "(";
@@ -36,3 +34,33 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+private:
+    void treeToString(string& ans, TreeNode* root) {
+        if(root == NULL) return;
+
+        ans += to_string(root -> val);
+        if(root -> right && !root -> left) ans += "()";
+        
+        if(root -> left) {
+            ans += "(";
+            treeToString(ans, root -> left);
+            ans += ")";
+        }
+        
+        if(root -> right) {
+            ans += "(";
+            treeToString(ans, root -> right);
+            ans += ")";
+        }
+    }
+public:
+    string tree2str(TreeNode* root) {
+        string ans = "";
+        treeToString(ans, root);
+        return ans;
+    }
+};
+*/
