ab5e8a55fd5f612cbe9ff0ca77915143bc43ade8
GVRS SAI GANESH <63167616+gvrs9@users.noreply.github.com>
2023-12-09 07:37:20 +0530

    Time: 3 ms (62.52%), Space: 12.7 MB (96.74%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<string> binaryTreePathsInString;
+    string path = "";
+    void binaryTreePathString(TreeNode* root) {
+        if(!root) {
+            binaryTreePathsInString.push_back(path);
+            return;
+        }
+
+        path += to_string(root -> val);
+
+        if(!root -> left && !root -> right) {
+            binaryTreePathsInString.push_back(path);
+            return;
+        }
+
+        if(root -> left) {
+            path += "->";
+            binaryTreePathString(root -> left);
+            // cout << path << "\n";
+            while(path.back() != '>') path.pop_back();
+            path.pop_back();path.pop_back();
+        }
+
+        if(root -> right) {
+            path += "->";
+            binaryTreePathString(root -> right);
+            // cout << path << "\n";
+            while(path.back() != '>') path.pop_back();
+            path.pop_back();path.pop_back();
+        }
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        binaryTreePathString(root);
+        return binaryTreePathsInString;
+    }
+};
